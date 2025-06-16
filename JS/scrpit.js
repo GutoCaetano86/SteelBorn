9e4c0c96a42be12134b36efb111fe4a8a2be7b8f
Augusto Caetano Maria <augusto.caetano86@gmail.com>
2025-06-16 10:25:47 -0300

    Ajustes arq

---
+++ JS/scrpit.js
@@ -0,0 +1,17 @@
+<script>
+        // Script para o menu mobile
+        const mobileMenuButton = document.getElementById('mobile-menu-button');
+        const mobileMenu = document.getElementById('mobile-menu');
+
+        mobileMenuButton.addEventListener('click', () => {
+            mobileMenu.classList.toggle('hidden');
+        });
+
+        // Fecha o menu mobile ao clicar em um link
+        const mobileLinks = document.querySelectorAll('#mobile-menu a');
+        mobileLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                mobileMenu.classList.add('hidden');
+            });
+        });
+    </script>
